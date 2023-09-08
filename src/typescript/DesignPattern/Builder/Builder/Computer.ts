ed725c2063dd524c9a7113d89576d247c0572781
Tim Dehler <tim_dehler@goodyear.com>
2023-09-08 14:52:28 +0200

    Builder Pattern tests missing

--- src/typescript/DesignPattern/Builder/Builder/Computer.ts
+++ src/typescript/DesignPattern/Builder/Builder/Computer.ts
@@ -1,15 +1,71 @@
 class Computer {
   public constructor(
-    public computerType: string,
-    public cpu: string,
-    public mainboard: string,
-    public ram: string,
-    public gpu: string,
-    public powersupply: string,
-    public memoryType: string,
-    public memorySize: string,
-    public hasBattery: boolean
+    private _computerType: string,
+    private _cpu: string,
+    private _mainboard: string,
+    private _ram: string,
+    private _gpu: string,
+    private _powersupply: string,
+    private _memoryType: string,
+    private _memorySize: string,
+    private _hasBattery: boolean
   ) {}
+
+  public get computerType(): string {
+    return this._computerType;
+  }
+  public set computerType(value: string) {
+    this._computerType = value;
+  }
+
+  public get hasBattery(): boolean {
+    return this._hasBattery;
+  }
+  public set hasBattery(value: boolean) {
+    this._hasBattery = value;
+  }
+  public get memorySize(): string {
+    return this._memorySize;
+  }
+  public set memorySize(value: string) {
+    this._memorySize = value;
+  }
+  public get memoryType(): string {
+    return this._memoryType;
+  }
+  public set memoryType(value: string) {
+    this._memoryType = value;
+  }
+  public get powersupply(): string {
+    return this._powersupply;
+  }
+  public set powersupply(value: string) {
+    this._powersupply = value;
+  }
+  public get gpu(): string {
+    return this._gpu;
+  }
+  public set gpu(value: string) {
+    this._gpu = value;
+  }
+  public get ram(): string {
+    return this._ram;
+  }
+  public set ram(value: string) {
+    this._ram = value;
+  }
+  public get mainboard(): string {
+    return this._mainboard;
+  }
+  public set mainboard(value: string) {
+    this._mainboard = value;
+  }
+  public get cpu(): string {
+    return this._cpu;
+  }
+  public set cpu(value: string) {
+    this._cpu = value;
+  }
 
   public toString(): string {
     return (
